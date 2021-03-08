d809f55af525d15011f449748a40970abd35c2fb
Matt Malone <mattagape@me.com>
2021-03-08 15:06:04 +0700

    Hard-coded id

--- app/controllers/hasura_controller.rb
+++ app/controllers/hasura_controller.rb
@@ -3,7 +3,7 @@ class HasuraController < ApplicationController
 	def register_user_handler
     request_data = params[:input]
     puts request_data
-    render json: request_data
+    render json: {id: 1} #request_data
   end
 
 end
