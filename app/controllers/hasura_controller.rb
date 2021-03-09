4e7f34bb810b85053cb8e570e88ba18ba10a4795
Matt Malone <mattagape@me.com>
2021-03-09 14:54:13 +0700

    Changed name to username

--- app/controllers/hasura_controller.rb
+++ app/controllers/hasura_controller.rb
@@ -1,23 +1,26 @@
 class HasuraController < ApplicationController
 
 	def register_user_handler
-    # request_data = params[:input]
-   #  user = User.new(user_params)
-   #  if user.save
-	  #   render json: user
-	  # else
-   #    begin
-   #      user.save!
-   #    rescue ActiveRecord::RecordInvalid => e
-   #      render json: {:code=>422, :message=>e}
-   #    else
-   #      render json: nil, status: :ok
-   #    end
-	  # end
+    request_data = params[:input]
+    user = User.new
+    user.username = request_data.username
+    user.email = request_data.email
+    user.password = request_data.password
+    if user.save
+	    render json: user
+	  else
+      begin
+        user.save!
+      rescue ActiveRecord::RecordInvalid => e
+        render json: {:code=>422, :message=>e}
+      else
+        render json: nil, status: :ok
+      end
+	  end
 
-		request_data = user_params #params[:input]
-    puts request_data
-    render json: request_data
+		# request_data = params[:input]
+  #   puts request_data
+  #   render json: request_data
 
   end
 
