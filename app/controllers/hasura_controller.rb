d09d8a06777280ed0571d84849711daac23baaf6
Matt Malone <mattagape@me.com>
2021-03-09 13:38:14 +0700

    Set up user save, etc

--- app/controllers/hasura_controller.rb
+++ app/controllers/hasura_controller.rb
@@ -2,8 +2,18 @@ class HasuraController < ApplicationController
 
 	def register_user_handler
     request_data = params[:input]
-    puts request_data
-    render json: {id: 1} #request_data
+    user = User.new(params[:input])
+    if user.save
+	    render json: user # {id: 1} #request_data
+	  else
+      begin
+        user.save!
+      rescue ActiveRecord::RecordInvalid => e
+        render json: {:code=>422, :message=>e}
+      else
+        render json: nil, status: :ok
+      end
+	  end
   end
 
 end
