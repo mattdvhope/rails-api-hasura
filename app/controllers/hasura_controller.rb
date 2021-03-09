4909491d34d870e641a4117d679f0e5699da1eb1
Matt Malone <mattagape@me.com>
2021-03-09 14:30:46 +0700

    Strong params

--- app/controllers/hasura_controller.rb
+++ app/controllers/hasura_controller.rb
@@ -2,18 +2,23 @@ class HasuraController < ApplicationController
 
 	def register_user_handler
     # request_data = params[:input]
-    user = User.new(user_params)
-    if user.save
-	    render json: user # {id: 1} #request_data
-	  else
-      begin
-        user.save!
-      rescue ActiveRecord::RecordInvalid => e
-        render json: {:code=>422, :message=>e}
-      else
-        render json: nil, status: :ok
-      end
-	  end
+   #  user = User.new(user_params)
+   #  if user.save
+	  #   render json: user # {id: 1} #request_data
+	  # else
+   #    begin
+   #      user.save!
+   #    rescue ActiveRecord::RecordInvalid => e
+   #      render json: {:code=>422, :message=>e}
+   #    else
+   #      render json: nil, status: :ok
+   #    end
+	  # end
+
+		request_data = params[:input]
+    puts request_data
+    render json: request_data
+
   end
 
   private
