b5ac0855d17644f6804940165df3e7ad93cdcef8
Matt Malone <mattagape@me.com>
2021-03-09 14:20:34 +0700

    Strong params

--- app/controllers/hasura_controller.rb
+++ app/controllers/hasura_controller.rb
@@ -19,7 +19,7 @@ class HasuraController < ApplicationController
   private
 
   def user_params
-    params[:input].require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
