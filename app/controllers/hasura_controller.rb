97c06c824a34a79b2a6ccfe96dfd55fe53434149
Matt Malone <mattagape@me.com>
2021-03-27 06:55:50 +0700

    Set update for user.login_time

--- app/controllers/hasura_controller.rb
+++ app/controllers/hasura_controller.rb
@@ -3,8 +3,7 @@ class HasuraController < ApplicationController
   def register_user_handler
     user = User.find_by_fb_id(input_params[:fb_id]) # maybe change to 'where' for better performance speed
     if user
-      update_login_time(user, input_params[:login_time])
-      user.save
+      user.update(login_time: input_params[:login_time])
       render json: user
     else
       create
@@ -31,10 +30,6 @@ class HasuraController < ApplicationController
 
     def input_params # ":input" is the param provided by hasura, NOT ":user"
       params.require(:input).permit(:fb_id, :first_name, :last_name, :name, :picture_url, :login_time)
-    end
-
-    def update_login_time(user, login_time)
-      user.login_time = login_time
     end
 
 end
