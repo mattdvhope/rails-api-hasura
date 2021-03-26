923791671759c8cc00575ca4d3e889d945be4f7e
Matt Malone <mattagape@me.com>
2021-03-26 20:32:38 +0700

    Added login_time to strong params

--- app/controllers/hasura_controller.rb
+++ app/controllers/hasura_controller.rb
@@ -3,6 +3,7 @@ class HasuraController < ApplicationController
   def register_user_handler
     user = User.find_by_fb_id(input_params[:fb_id]) # maybe change to 'where' for better performance speed
     if user
+      update_login_time(user, input_params[:login_time])
       render json: user
     else
       create
@@ -27,7 +28,12 @@ class HasuraController < ApplicationController
   private
 
     def input_params # ":input" is the param provided by hasura, NOT ":user"
-      params.require(:input).permit(:fb_id, :first_name, :last_name, :name, :picture_url)
+      params.require(:input).permit(:fb_id, :first_name, :last_name, :name, :picture_url, :login_time)
+    end
+
+    def update_login_time(user, login_time)
+      user.login_time = login_time
+      user.save
     end
 
 end
